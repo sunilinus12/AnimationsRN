389f55c5afb9c0d83970e51c004eaa117e522522
sunilinus12 <www.suniljaunwal2@gmail.com>
2022-10-10 15:27:08 +0530

    timing function

--- App.tsx
+++ App.tsx
@@ -1,12 +1,40 @@
-import {StyleSheet, Text, View} from 'react-native';
-import React from 'react';
+import {Animated, StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+import React, {useState} from 'react';
 
 export default function App() {
+  const value = useState(
+    new Animated.ValueXY({
+      x: 0,
+      y: 0,
+    }),
+  )[0];
+  function moveBall() {
+    Animated.timing(value, {
+      toValue: {x: 100, y: 100},
+      duration: 100,
+      useNativeDriver: false,
+    }).start();
+  }
   return (
-    <View>
-      <Text>App</Text>
+    <View style={styles.container}>
+      <Animated.View style={[styles.box, value.getLayout()]} />
+      <TouchableOpacity onPress={() => moveBall()} activeOpacity={0.5}>
+        <Text>App</Text>
+      </TouchableOpacity>
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  box: {
+    width: 100,
+    height: 100,
+    backgroundColor: 'green',
+  },
+});
